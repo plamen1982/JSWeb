ebe4c20cc7ac465bb50d9dcce077d509ec1112f9
“[plamen1982]” <plamen.hristov@ymail.com>
2019-01-16 23:49:20 +0200

    added view

---
+++ ExpressJS/Exercises/TheLab/handlers/index.js
@@ -0,0 +1,2 @@
+const handler = require("./home");
+module.exports = { handler };
--- ExpressJS/Exercises/TheLab/index.js
+++ ExpressJS/Exercises/TheLab/index.js
@@ -1,13 +1,9 @@
 const http = require('http');
 const port = 3000;
+const home = require('./handlers');
 
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    })
-
-    res.write('Hello');
-    res.end();
+    home.handler(req, res);
 }).listen(port, () => {
     console.log(`The server is listening on port: ${port}`)
 });
