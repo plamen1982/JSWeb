0f139642b0c70b81f880c5edc9b41e3cf743ec0d
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-03 00:30:28 +0200

    Adding hashing to custom cookite

--- ExpressJS/Exercises/Seesions-Auth-Playground/sessionAuth/routes/index.js
+++ ExpressJS/Exercises/Seesions-Auth-Playground/sessionAuth/routes/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const crypto = require('crypto');
 /* GET home page. */
+
+function generateSalt() {
+  return crypto.randomBytes(128).toString('base64');
+}
+
+function generateHashPassword(salt, password) {
+  return crypto.createHmac('sha256', salt).update(password).digest('hex');
+}
+
 router.get('/', (req, res, next) => {
 
   res.render('index', { title: 'Express' });
 });
 
 router.get('/setCookie', (req, res, next) => {
-  res.cookie('name', 'tobi', { path: '/setCookie'})
+  let salt = generateSalt();
+  let hashedPass = generateHashPassword(salt, 'tobi');
+  console.log(salt);
+  console.log(hashedPass);
+  res.cookie('name', hashedPass, { path: '/setCookie', maxAge: 1000000})
   res.send('the cookie is set baby')
 });
 
