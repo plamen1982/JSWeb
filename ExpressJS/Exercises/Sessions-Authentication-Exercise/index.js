5213bb20f81749a1de5a5ae9031d6f0375b9ae68
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-03 18:03:02 +0200

    Created routes without handlers

--- ExpressJS/Exercises/Sessions-Authentication-Exercise/index.js
+++ ExpressJS/Exercises/Sessions-Authentication-Exercise/index.js
@@ -1,9 +1,10 @@
 const env = process.env.NODE_ENV || 'development';
-
-const config = require('./config/config')[env];
-require('./config/database')(config);
 const app = require('express')();
+const config = require('./config/config')[env];
+
+require('./config/database')(config);
 require('./config/express')(app);
 require('./config/routes')(app);
 require('./config/passport')();
+
 app.listen(config.port);
