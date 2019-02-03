fa997269df4fa156d40eb228004d629657897089
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-03 11:02:08 +0200

    JWT example added

---
+++ ExpressJS/Exercises/JWT/index.js
@@ -0,0 +1,45 @@
+//TODO: try to remove 'use strict' after tutorial works
+'use strict';
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+
+// PAYLOAD
+const payload = {
+    myPersonalTestData: 'My personal test data',
+    myPersonalTestData1: 'My personal test data1',
+    myPersonalTestData2: 'My personal test data2',
+}
+
+// PRIVATE and PUBLIC key
+
+const privateKEY = fs.readFileSync('./private.key', 'utf8');
+const publicKEY = fs.readFileSync('./public.key', 'utf8');
+
+const i  = 'Mysoft corp';          // Issuer 
+const s  = 'some@user.com';        // Subject 
+const a  = 'http://mysoftcorp.in'; // Audience
+
+// SIGNING OPTIONS
+const signOptions = {
+ issuer:  i,
+ subject:  s,
+ audience:  a,
+ expiresIn:  "12h",
+ algorithm:  "HS256"
+};
+
+let token = jwt.sign(payload, privateKEY, signOptions);
+// console.log(token)
+// const verifyOptions = {
+//     issuer:  i,
+//     subject:  s,
+//     audience:  a,
+//     expiresIn:  "12h",
+//     algorithm:  ["HS256"],
+// };
+
+// const legit = jwt.verify(token, verifyOptions, verifyOptions);
+// console.log(legit)
+
+var decoded =  jwt.decode(token, {complete: true});
+console.log("\nDecoded jwt: "+ JSON.stringify(decoded));
