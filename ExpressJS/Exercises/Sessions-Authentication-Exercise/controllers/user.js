5966bedc3079f67bf43bed6023018e4e050a87a5
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-04 08:23:24 +0200

    added functionality for registration

--- ExpressJS/Exercises/Sessions-Authentication-Exercise/controllers/user.js
+++ ExpressJS/Exercises/Sessions-Authentication-Exercise/controllers/user.js
@@ -1,9 +1,41 @@
+const User = require('../models/User');
+const encryption = require('../util/encryption');
+
 module.exports = {
     registerGet: (req, res) => {
         res.render('user/register');
     },
     registerPost: async (req, res) => {
-       // TODO:
+        const reqUser = req.body;
+        const { username, password, repeatPassword, firstName, lastName} = reqUser;
+        const salt = encryption.generateSalt();
+        if(password !== repeatPassword) {
+            res.locals.error = 'Password does not matched.'
+            res.render('user/register', reqUser);
+        }
+        const hashedPass = encryption.generateHashedPassword(salt, password);
+        try {
+            const user = await User.create({
+                username,
+                hashedPass,
+                salt,
+                firstName,
+                lastName,
+                roles: ['user'],
+            });
+            req.logIn(user, (error, user) => {
+                if(error) {
+                    res.locals.error = error;
+                    res.render('users/register', user);
+                } else {
+                    res.redirect('/');
+                }
+            })
+        } catch(error) {
+            console.log(error);
+            res.locals.error = error;
+            res.render('users/register');
+        }
     },
     logout: (req, res) => {
         // TODO:
