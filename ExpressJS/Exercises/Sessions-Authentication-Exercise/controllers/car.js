7f33b73ce3b1e1737c6f752f45dc75fcc62c9eb9
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-04 10:41:29 +0200

    methods in car controller created without implementation

---
+++ ExpressJS/Exercises/Sessions-Authentication-Exercise/controllers/car.js
@@ -0,0 +1,27 @@
+const Car = require('../models/Car');
+const Rent = require('../models/Rent');
+const User = require('../models/User');
+
+module.exports = {
+    createCar: (req, res) => {
+
+    },
+    allCars: (req, res) => {
+
+    },
+    rentCarDetals: (req, res) => {
+
+    },
+    rentCar: (req, res) => {
+
+    },
+    myRentedCars: (req, res) => {
+
+    },
+    editedCarView: (req, res) => {
+
+    },
+    editedCar: (req, res) => {
+        
+    },
+};
