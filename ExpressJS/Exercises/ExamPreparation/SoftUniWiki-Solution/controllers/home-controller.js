73230f66106158b71171780211e8c32fdf945864
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-09 21:53:23 +0200

    find last 3 articles and show it to index

--- ExpressJS/Exercises/ExamPreparation/SoftUniWiki-Solution/controllers/home-controller.js
+++ ExpressJS/Exercises/ExamPreparation/SoftUniWiki-Solution/controllers/home-controller.js
@@ -1,5 +1,31 @@
+const Article = require('../models/Article');
 module.exports = {
     index: (req, res) => {
-        res.render('home/index');
+        Article
+            .find()
+            .sort({ creationDate: 'descending' })
+            .limit(3)
+            .then((articles) => {
+                const lastArticle = articles[0];
+                if(!lastArticle) {
+                    res.render('home/index', 
+                    {
+                        title: 'No title',
+                        content: 'No content',
+                    });
+                    return;
+                }
+                const context = {
+                    title: lastArticle.title,
+                    content: lastArticle.content
+                        .split(' ')
+                        .slice(0, 50)
+                        .join(' '),
+                    _id: lastArticle._id,
+                    latest: articles.map(a => { return { _id: a._id, title: a.title } }),
+                }
+                res.render('home/index', context);
+            })
+            .catch(console.error);
     }
 };
