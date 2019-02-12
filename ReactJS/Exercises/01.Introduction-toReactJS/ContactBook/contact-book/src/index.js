3fc369f70e705350b7d26f48ff1c5e19b6cbc942
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-12 21:31:30 +0200

    created nested component

--- ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/index.js
+++ ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const WelcomeStranger = () => (
+    <h1>Welcome, stranger!</h1>
+);
+
+const HelloDearFellow = () => (
+    <h2>Hello from dear fellow!</h2>
+);
+
+const ComponentBlender = () => (
+    <div>
+        <WelcomeStranger />
+        <HelloDearFellow />
+    </div>
+);
 
 
-
-ReactDOM.render(React.createElement("div", {}, "Hello, World!"), document.getElementById('root'));
+ReactDOM.render(<ComponentBlender />, document.getElementById('root'));
 
 
