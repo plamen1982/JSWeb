1b0a607ba8fa49602776bc574087954c73ccbfb1
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-12 21:15:15 +0200

    create Hello world react component

---
+++ ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HelloMessage = (props) => (
+  <div>Hello {props.name}</div>
+)
+
+export default HelloMessage;
---
+++ ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Hello from './App';
+
+
+ReactDOM.render(<Hello name="world" />, document.getElementById('root'));
+
+
