da8222a6062314fe861b556f89b55dce138ffad6
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-12 23:45:12 +0200

    added static DetailsSection component

--- ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/index.js
+++ ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/index.js
@@ -5,27 +5,44 @@ import "./style/app.css";
 
 //reuse MainHeader to become just Header
 const HeaderMain = () => <header>&#9993; Contact Book</header>;
-const DetailsHeader = () => <div />;
 
 class ContactList extends Component {
   render() {
     return (
       <div id="list">
         <h1>Contacts</h1>
-        <div className="content">
-          {this.props.children}
-        </div>
+        <div className="content">{this.props.children}</div>
       </div>
     );
   }
-} 
+}
+
 const ContactCard = () => (
   <div className="contact" data-id="id">
     <span className="avatar small">&#9787;</span>
     <span className="title">Maria Petrova</span>
   </div>
 );
-const DetailsSection = () => <div />;
+const DetailsSection = () => (
+  <div id="details">
+    <h1>Details</h1>
+    <div className="content">
+      <div className="info">
+        <div className="col">
+          <span className="avatar">&#9787;</span>
+        </div>
+        <div className="col">
+          <span className="name">Ivan</span>
+          <span className="name">Ivanov</span>
+        </div>
+      </div>
+      <div className="info">
+        <span className="info-line">&phone; 0887 123 456</span>
+        <span className="info-line">&#9993; i.ivanov@gmail.com</span>
+      </div>
+    </div>
+  </div>
+);
 
 const Page = () => (
   <div className="container">
@@ -34,26 +51,9 @@ const Page = () => (
       <ContactList>
         <ContactCard />
       </ContactList>
-      <div id="details">
-        <h1>Details</h1>
-        <div className="content">
-          <div className="info">
-            <div className="col">
-              <span className="avatar">&#9787;</span>
-            </div>
-            <div className="col">
-              <span className="name">Ivan</span>
-              <span className="name">Ivanov</span>
-            </div>
-          </div>
-          <div className="info">
-            <span className="info-line">&phone; 0887 123 456</span>
-            <span className="info-line">&#9993; i.ivanov@gmail.com</span>
-          </div>
-        </div>
-      </div>
+      <DetailsSection />
+      <footer>Contact Book SPA &copy; 2017</footer>
     </div>
-    <footer>Contact Book SPA &copy; 2017</footer>
   </div>
 );
 
