b2a0fe9f55e85223e9253d819235c855b89fc03f
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-13 20:38:59 +0200

    move components from index.js to App.js

--- ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/App.js
+++ ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/App.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = (props) => (
-  <div>Hello {props.name}</div>
+const Header = () => <header>&#9993; Contact Book</header>;
+
+class ContactList extends Component {
+  render() {
+    return (
+      <div id="list">
+        <h1>Contacts</h1>
+        <div className="content">{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+const ContactCard = () => (
+  <div className="contact" data-id="id">
+    <span className="avatar small">&#9787;</span>
+    <span className="title">Maria Petrova</span>
+  </div>
+);
+
+const DetailsSection = () => (
+  <div id="details">
+    <h1>Details</h1>
+    <div className="content">
+      <div className="info">
+        <div className="col">
+          <span className="avatar">&#9787;</span>
+        </div>
+        <div className="col">
+          <span className="name">Ivan</span>
+          <span className="name">Ivanov</span>
+        </div>
+      </div>
+      <div className="info">
+        <span className="info-line">&phone; 0887 123 456</span>
+        <span className="info-line">&#9993; i.ivanov@gmail.com</span>
+      </div>
+    </div>
+  </div>
+);
+
+const Footer = () => (
+  <footer>Contact Book SPA &copy; 2019</footer>
+);
+
+const App = () => (
+  <div className="container">
+    <Header />
+    <div id="book">
+      <ContactList>
+        <ContactCard />
+      </ContactList>
+      <DetailsSection />
+    </div>
+    <Footer />
+  </div>
 );
 
 export default App;
--- ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/index.js
+++ ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/index.js
@@ -1,65 +1,7 @@
-import React, { Component } from "react";
+import React  from "react";
 import ReactDOM from "react-dom";
 import "./style/index.css";
 import "./style/app.css";
+import App from "./App";
 
-//reuse MainHeader to become just Header
-const HeaderMain = () => <header>&#9993; Contact Book</header>;
-
-class ContactList extends Component {
-  render() {
-    return (
-      <div id="list">
-        <h1>Contacts</h1>
-        <div className="content">{this.props.children}</div>
-      </div>
-    );
-  }
-}
-
-const ContactCard = () => (
-  <div className="contact" data-id="id">
-    <span className="avatar small">&#9787;</span>
-    <span className="title">Maria Petrova</span>
-  </div>
-);
-
-const DetailsSection = () => (
-  <div id="details">
-    <h1>Details</h1>
-    <div className="content">
-      <div className="info">
-        <div className="col">
-          <span className="avatar">&#9787;</span>
-        </div>
-        <div className="col">
-          <span className="name">Ivan</span>
-          <span className="name">Ivanov</span>
-        </div>
-      </div>
-      <div className="info">
-        <span className="info-line">&phone; 0887 123 456</span>
-        <span className="info-line">&#9993; i.ivanov@gmail.com</span>
-      </div>
-    </div>
-  </div>
-);
-
-const Footer = () => (
-  <footer>Contact Book SPA &copy; 2017</footer>
-);
-
-const Page = () => (
-  <div className="container">
-    <HeaderMain />
-    <div id="book">
-      <ContactList>
-        <ContactCard />
-      </ContactList>
-      <DetailsSection />
-    </div>
-    <Footer />
-  </div>
-);
-
-ReactDOM.render(<Page />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
