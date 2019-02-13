a781e25bd458e5d89234067a06b354d85d7021f1
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-13 23:45:17 +0200

    Contact List Task solved

--- ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/App.js
+++ ReactJS/Exercises/01.Introduction-toReactJS/ContactBook/contact-book/src/App.js
@@ -5,7 +5,7 @@ const Header = () => <header>&#9993; Contact Book</header>;
 
 const Footer = () => <footer>Contact Book SPA &copy; 2019</footer>;
 
-const DetailsSection = () => (
+const DetailsSection = ({ contact }) => (
   <div id="details">
     <h1>Details</h1>
     <div className="content">
@@ -14,24 +14,34 @@ const DetailsSection = () => (
           <span className="avatar">&#9787;</span>
         </div>
         <div className="col">
-          <span className="name">Ivan</span>
-          <span className="name">Ivanov</span>
+          <span className="name">{contact.firstName}</span>
+          <span className="name">{contact.lastName}</span>
         </div>
       </div>
       <div className="info">
-        <span className="info-line">&phone; 0887 123 456</span>
-        <span className="info-line">&#9993; i.ivanov@gmail.com</span>
+        <span className="info-line">&phone; {contact.phone}</span>
+        <span className="info-line">&#9993; {contact.email}</span>
       </div>
     </div>
   </div>
 );
 
 class ContactList extends Component {
-  handleOnClick(contact) {
-    console.log(contact);
+  state = {
+    contact : {
+      firstName: "Maria",
+      lastName: "Ivanov",
+      phone: "0888 123 456",
+      email: "i.ivanov@gmail.com"
+    }
+  }
+  handleOnClick = (currentContact) => {
+    console.log(currentContact)
+    this.setState({ contact: currentContact })
   }
 
   render() {
+
     const contactList = contacts.map(contact => (
       <ContactCard
         key={contact.email}
@@ -41,9 +51,12 @@ class ContactList extends Component {
     ));
 
     return (
-      <div id="list">
-        <h1>Contacts</h1>
-        <div className="content">{contactList}</div>
+      <div>
+        <div id="list">
+          <h1>Contacts</h1>
+          <div className="content">{contactList}</div>
+        </div>
+        <DetailsSection contact={this.state.contact}/>
       </div>
     );
   }
@@ -52,6 +65,7 @@ class ContactList extends Component {
 class ContactCard extends Component {
     constructor(props) {
       super(props);
+      this.state = {};
 
       this.handleClick = this.handleClick.bind(this);
     }
@@ -65,9 +79,9 @@ class ContactCard extends Component {
     const { contact } = this.props;
 
     return (
-      <div className="contact" data-id="id">
+      <div className="contact" data-id="id" onClick={this.handleClick}>
         <span className="avatar small">&#9787;</span>
-        <a href="/" onClick={this.handleClick}><span className="title">{contact.firstName}</span></a>
+        <span className="title">{contact.firstName}</span>
       </div>
     );
   }
@@ -78,7 +92,6 @@ const App = () => (
     <Header />
     <div id="book">
       <ContactList />
-      <DetailsSection />
     </div>
     <Footer />
   </div>
