474abe3da8fcc19a57c7338e5c8de35ffa084abd
plamen1982 <plamen.hristov@ymail.com>
2019-02-22 15:45:50 +0200

    increment and decrement functionality added

--- ReactJS/Exercises/Random/forms-and-events/src/components/counter/counter.js
+++ ReactJS/Exercises/Random/forms-and-events/src/components/counter/counter.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 
+const DisplayCounter = (props) => (
+    <div>    
+        <div className="column docs-icon-set-column">
+            <i aria-hidden="true" className="clock big icon"></i>
+            <span className="ui label">{props.counter}</span>
+        </div>
+    </div>
+
+);
+
 class Counter extends Component {
     constructor(props) {
         super(props);
@@ -7,20 +17,31 @@ class Counter extends Component {
         this.state = {
             counter: 0,
         }
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
     }
+
+    increment() {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+
+    decrement() {
+        let { counter } = this.state;
+        this.setState({
+            counter: this.state.counter - 1
+        })
+    } 
 
     render() {
         const { counter } = this.state;
         return(
             <div className="ui container">
-                <div className="column docs-icon-set-column">
-                 <i aria-hidden="true" class="clock big icon"></i>
-                    <span className="name">{counter}</span>
-                </div>
-                <button className="ui button">+</button>
-                <button className="ui button">-</button>
+                <DisplayCounter counter={counter} />
+                <button className="ui button" onClick={this.decrement}>-</button>
+                <button className="ui button" onClick={this.increment}>+</button>
             </div>
-
         );
     }
 }
