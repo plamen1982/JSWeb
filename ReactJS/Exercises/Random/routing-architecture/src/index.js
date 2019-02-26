1b8194ceb9219993c6f3d160dec5551347e01dcb
plamen1982 <plamen.hristov@ymail.com>
2019-02-26 22:10:37 +0200

    created router with route home

--- ReactJS/Exercises/Random/routing-architecture/src/index.js
+++ ReactJS/Exercises/Random/routing-architecture/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Router is deployed at the most upper part of the app
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const HomeComponent = () => (<h1>Home</h1>)
+
+const AppWrapper = () => {
+    return (
+        <Router>
+            <Route path="/" component={HomeComponent} />
+        </Router>
+    );
+}
+ReactDOM.render(
+    <AppWrapper />
+    , document.getElementById('root'));
