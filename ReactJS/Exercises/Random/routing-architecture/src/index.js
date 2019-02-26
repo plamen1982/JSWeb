092d3d923e0d220e1dbec0ca5065a4a416f05a2d
plamen1982 <plamen.hristov@ymail.com>
2019-02-26 22:17:18 +0200

    added switch wrapper to the route components

--- ReactJS/Exercises/Random/routing-architecture/src/index.js
+++ ReactJS/Exercises/Random/routing-architecture/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 //Router is deployed at the most upper part of the app
 
-const HomeComponent = () => (<h1>Home</h1>)
+const HomeComponent = () => (<h1>Home</h1>);
+const AboutComponent = () => (<h1>About</h1>);
+const ContactComponent = () => (<h1>Contact</h1>);
 
 const AppWrapper = () => {
     return (
         <Router>
-            <Route path="/" component={HomeComponent} />
+            <Switch>
+                <Route path="/" component={HomeComponent} exact/>
+                <Route path="/about" component={AboutComponent} exact/>
+                <Route path="/contact" component={ContactComponent} exact/>
+            </Switch>
         </Router>
     );
 }
 ReactDOM.render(
     <AppWrapper />
-    , document.getElementById('root'));
+    ,document.getElementById('root'));
