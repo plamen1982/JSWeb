2c9af39f1230cdbf35a14328b5718125fa4b08c8
plamen1982 <plamen.hristov@ymail.com>
2019-03-04 12:53:15 +0200

    added star-wars service to fetch data from star wars api

---
+++ ReactJS/Exercises/Random/hoc-and-context-api/services/star-wars-service.js
@@ -0,0 +1,14 @@
+class StarWarsService {
+    constructor() {
+        this.baseUrl = 'https://swapi.co/api';
+        this.charactersUrl = `${this.baseUrl}/people`;
+    }
+
+    getStarWarsCharacters(page = 1) {
+        return fetch(`${this.charactersUrl}?page=${page}`)
+            .then(response => response.json())
+            .then(data => data.results);
+    }
+}
+
+export default StarWarsService;
