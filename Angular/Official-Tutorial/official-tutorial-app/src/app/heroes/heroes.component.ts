8e10fa9c931a9fb1fc74d0d349059a998193f2ff
plamen1982 <plamen.hristov@ymail.com>
2019-04-11 01:37:42 +0300

    listed heroes with *ngFor directive

--- Angular/Official-Tutorial/official-tutorial-app/src/app/heroes/heroes.component.ts
+++ Angular/Official-Tutorial/official-tutorial-app/src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from "../hero";
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,10 +10,7 @@ export class HeroesComponent implements OnInit {
 
   constructor() { }
 
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  }
+  heroes = HEROES;
 
   ngOnInit() {
   }
