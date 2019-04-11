c7932d805f6c014990daff0136eae2b224328809
plamen1982 <plamen.hristov@ymail.com>
2019-04-12 02:44:39 +0300

    added service method getHero by id from params and hero-deails creaeted as routable component

--- Angular/Official-Tutorial/official-tutorial-app/src/app/heroes/heroes.component.ts
+++ Angular/Official-Tutorial/official-tutorial-app/src/app/heroes/heroes.component.ts
@@ -13,11 +13,6 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   heroes: Hero[];
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
