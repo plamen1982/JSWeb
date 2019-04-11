8cf6fda2aace4cec1987de7775047ecb0ad7e3dc
plamen1982 <plamen.hristov@ymail.com>
2019-04-11 03:37:56 +0300

    created hero service and inject in heroes component

--- Angular/Official-Tutorial/official-tutorial-app/src/app/heroes/heroes.component.ts
+++ Angular/Official-Tutorial/official-tutorial-app/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +10,21 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  }
+
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
